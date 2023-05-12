52a4f687395d7f8f84b480a1026bdc1fa8c69e7e
K_aLex-a <shiotoy@icloud.com>
2023-05-12 22:49:47 +0300

    added second option via Math.Pow library

--- Seminar01_task001_(group)/Program.cs
+++ Seminar01_task001_(group)/Program.cs
@@ -5,6 +5,8 @@
 // а = 2, b = 10 -> нет
 // а = -3, b = 9 -> нет
 
+
+// Вариант 1 (в лоб):
 Console.Write("Введите число 1: ");                         // запрос на ввод первого числа
 int number1 = Convert.ToInt32(Console.ReadLine());          // прием первого числа
 
@@ -20,3 +22,21 @@ else
 {
     Console.Write($"a = {number1}, b = {number2} -> нет");  // иначе выводим на печать "нет"
 }
+
+
+// Вариант 2 (через библу Math):
+Console.Write("Input number 1: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 2: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+
+int pow = Convert.ToInt32(Math.Pow(num_b, 2));
+if (pow == num_a)
+{
+    Console.Write($"a = {num_a}, b = {num_b} -> yes");   // если равен, то выводим на печать "да"
+}
+else
+{
+    Console.Write($"a = {num_a}, b = {num_b} -> no");   // если равен, то выводим на печать "да"
+}
+
